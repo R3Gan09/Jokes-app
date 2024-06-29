5af5083124013c8e32abbd6c5a4d613f1729e569
R3Gan09 <152863340+R3Gan09@users.noreply.github.com>
2024-06-29 14:14:30 +0800

    Initial Commit

---
+++ Jokes app/Jokes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_appApp.swift
+//  Jokes app
+//
+//  Created by Regan on 29/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
